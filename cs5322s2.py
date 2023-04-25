5b58e2b3e1158ff77216bc7f19002733249e613c
Sam Yao <yaosam.521@gmail.com>
2023-04-24 21:19:23 -0500

    Added PCA reduction and cross validation - looking good so far

--- cs5322s2.py
+++ cs5322s2.py
@@ -1,14 +1,16 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
 '''
 Find information on WSD from Lexical Semantics (slides) and Chapter 23 (textbook)
 '''
 # Comment 1
-def WSD_test_rubbish(text,model):
+def WSD_test_rubbish(text):
+    vectorizer = TfidfVectorizer(stop_words='english')
+    rubbish_matrix = vectorizer.fit_transform(text)
+    df_rubbish = pd.DataFrame(rubbish_matrix.todense(), columns=vectorizer.get_feature_names_out())
     results = list()
     #preprocess the text
-    for sentence in text:
-        #classify word in sentence (load model in here)
-        prediction = model.predict(sentence)
-        results.append(prediction)
     return results
 
 def WSD_test_tissue(text,model):
--- training.py
+++ training.py
@@ -1,11 +1,12 @@
 #use this file to train the ML Model
 from sklearn import svm
-from sklearn.linear_model import LogisticRegression
-from nltk.corpus import wordnet as wn
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.model_selection import KFold
+import numpy as np
 
 corpus = list()
 target_column = list()
@@ -29,14 +30,44 @@ df['target_sense'] = target_column
 X = df.loc[:, df.columns != 'target_sense']
 y = df['target_sense']
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, stratify=y)
-#svm = svm()
+#PCA reduction to 7 components
+pca = PCA(n_components=7)
+pca.fit(X)
 
-lr = LogisticRegression()
+X_pca = pca.transform(X)
 
-lr.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X_pca,y, test_size=0.2, stratify=y,shuffle=True)
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+y_test_hat = clf.predict(X_test)
 
-y_test_hat = lr.predict(X_test)
-print(accuracy_score(y_test,y_test_hat))
+kf = KFold(n_splits=4, shuffle=True)
+acc_train=[]
+acc_test=[]
+for fold, (train_i, test_i) in enumerate(kf.split(X_pca)):
+    X_train, X_test = X_pca[train_i], X_pca[test_i]
+    y_train, y_test = y[train_i], y[test_i]
+    clf.fit(X_train, y_train)
+    y_hat_train = clf.predict(X_train)
+    train_acc = accuracy_score(y_train, y_hat_train)
+    acc_train.append(train_acc)
+    
+    y_hat_test = clf.predict(X_test)
+    test_acc = accuracy_score(y_test, y_hat_test)
+    acc_test.append(test_acc)
 
-#print(lr.predict_proba([0,0,0.34]))
+avg_tr=0 
+avg_te=0
+for tr_rec, te_rec in zip(acc_train, acc_test):
+    avg_tr += np.mean(tr_rec)
+    avg_te += np.mean(te_rec)
+avg_tr =avg_tr/len(acc_train)
+avg_te = avg_te/len(acc_test)
+print('Average Accuracy Train:{:0.2f} Accuracy Test:{:0.2f}'.format(avg_tr,avg_te))
+#test = TfidfVectorizer(stop_words='english').fit_transform("I needed to cry so I took out a tissue from the tissue box.")
+#df = pd.DataFrame(test.todense(), columns=vectorizor.get_feature_names_out())
+#print(df)
+
+#print(clf.predict(test_sentence))
+
+#clf.sa
